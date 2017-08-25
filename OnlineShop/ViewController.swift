3a561a41809b0abe9af01999b2c04731e357e2ee
Dehelean Andrei <AndiD@AD-MacBook.local>
2017-08-25 21:01:18 +0300

    basic project setup

---
+++ OnlineShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnlineShop
+//
+//  Created by Dehelean Andrei on 8/25/17.
+//  Copyright © 2017 Dehelean Andrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
